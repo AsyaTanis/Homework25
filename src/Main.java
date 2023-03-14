08737b908d99da3ceafbefa54e07f55010070e83
Анастасия <tasyaosipova736@gmail.com>
2023-03-14 09:12:00 +0300

    Stream API и Optional

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
 public class Main {
+
+    public static List<Integer> integerList = new ArrayList<>(List.of(
+            101, 91, 15, 20, 52, 33, 101, 207, 11, 666));
+
+    public static Long getCountForEvenInteger(List<Integer> integers) {
+        return integers.stream().filter(integer -> integer % 2 == 0).count();
+    }
+
+    public static <A> void findMinMax(
+            Stream<? extends A> stream,
+            Comparator<? super A> order,
+            BiConsumer<? super A, ? super A> minMaxConsumer) {
+
+        List<A> items = (List<A>) stream.sorted(order).collect(Collectors.toList());
+
+        if (!items.isEmpty()) {
+            minMaxConsumer.accept(items.get(0), items.get(items.size() - 1));
+        } else {
+            minMaxConsumer.accept(null, null);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        findMinMax(integerList.stream(), Integer::compareTo, (x, y) -> System.out.printf("min число: %s, max число: %s\n", x, y));
+        System.out.println("Количество четных чисел: " + getCountForEvenInteger(integerList));
     }
 }
